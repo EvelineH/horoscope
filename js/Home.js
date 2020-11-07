b5221115029c49cb663cec849b71341c0ea2e4eb
EvelineH <nkhur17@freeuni.edu.ge>
2020-11-07 20:04:12 +0400

    add daily grid

--- js/Home.js
+++ js/Home.js
@@ -76,6 +76,59 @@ export default class extends AbstractView {
             <li><a href="/horoscope/game" class="nav__link" data-link>Guess Who ?</a></li>
         </ul>
     </nav>
+
+    <p>Daily Horoscopes</p>
+    <div class="grid-container">
+        <div class="grid-item">
+            <p>Aries</p>
+            <p>Mar 21 - Apr 19</p>
+        </div>
+        <div class="grid-item">
+            <p>Taurus</p>
+            <p>Apr 20 - May 20</p>
+        </div>
+        <div class="grid-item">
+            <p>Gemini</p>
+            <p>May 21 - Jun 20</p>
+        </div>  
+        <div class="grid-item">
+            <p>Cancer</p>
+            <p>Jun 21 - Jul 22</p>
+        </div>
+        <div class="grid-item">
+            <p>Leo</p>
+            <p>Jul 23 - Aug 22</p>
+        </div>
+        <div class="grid-item">
+            <p>Virgo</p>
+            <p>Aug 23 - Sep 22</p>
+        </div>  
+        <div class="grid-item">
+            <p>Libra</p>
+            <p>Sep 23 - Oct 22</p>
+        </div>
+        <div class="grid-item">
+            <p>Scorpio</p>
+            <p>Oct 23 - Nov 21</p>
+        </div>
+        <div class="grid-item">
+            <p>Sagittarius</p>
+            <p>Nov 22 - Dec 21</p>
+        </div>
+        <div class="grid-item">
+            <p>Capricorn</p>
+            <p>Dec 22 - Jan 19</p>
+        </div>
+        <div class="grid-item">
+            <p>Aquarius</p>
+            <p>Jan 20 - Feb 18</p>
+        </div>
+        <div class="grid-item">
+            <p>Pisces</p>
+            <p>Feb 19 - March 20</p>
+        </div>  
+    </div>
+
 </div>`;
   }
 }
